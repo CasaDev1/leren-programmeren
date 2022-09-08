bde7660cceeda8e8b34f4cd23576a9f2f354eda5
CaSa078 <113099680+CaSa078@users.noreply.github.com>
2022-09-08 15:46:12 +0200

    Add files via upload
    dit zijn aantekeningen

---
+++ spiekbriefje.python.py
@@ -0,0 +1,35 @@
+# commentaar
+# 2 regels
+# regel 3
+# getal met comma is een float
+
+####################################################################
+# variable
+####################################################################
+# variable
+aantal = 6 
+#interter = geheelgetal
+#string = zin of een word
+####################################################################
+# beeld in functie
+####################################################################
+
+#type casting 
+#print(str(aantal) + ' ' +omschrijving)
+#>>> aantal = 3
+#>>> omschrijving = 'Croky naturel 300gr'
+#>>> stuksprijs = 0
+>>> stuksprijs = 0.89
+>>> print('nadir')
+nadir
+>>> print( aantal * stuksprijs)
+2.67
+>>> print( aantal * omschrijving)
+Croky naturel 300grCroky naturel 300grCroky naturel 300gr
+>>> print( aantal + omschrijving)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+
+>>> print(str(aantal) + ' ' +omschrijving)
+3 Croky naturel 300gr
+print(str(aantal) + ' ' + omschrijving + ' ' + str(stuksprijs))
