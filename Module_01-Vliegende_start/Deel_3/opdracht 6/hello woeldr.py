3fd1a47653038519446d07b65948df9566a8fbb6
CaSa078 <99073112@mydavinci.nl>
2022-09-09 11:08:31 +0200

    map erbij

---
+++ Module_01-Vliegende_start/Deel_3/opdracht 6/hello woeldr.py
@@ -0,0 +1,15 @@
+
+
+
+hello= """Hello wereld van…                                    
+
+    pppp    y     y   ttttttt   h   h    oooo   nnnn       
+    p   p    y   y       t      h   h   o    o  n   n
+    p   p     y y        t      h   h   o    o  n   n
+    pppp       y         t      hhhhh   o    o  n   n
+    p          y         t      h   h   o    o  n   n
+    p          y         t      h   h   o    o  n   n
+    p          y         t      h   h    oooo   n   n"""
+    
+print(hello)
+#Nadir 99073112
