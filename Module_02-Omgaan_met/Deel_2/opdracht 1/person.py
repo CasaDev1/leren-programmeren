d13077b19769436991840e29c0ca852b41bb0e36
CaSa078 <99073112@mydavinci.nl>
2022-09-12 13:04:13 +0200

    nieuw opdracht

---
+++ Module_02-Omgaan_met/Deel_2/opdracht 1/person.py
@@ -0,0 +1,10 @@
+adres = """----------------------------------------------------
+|  Naam      : Nadir Ait                
+|  Adres     : Koekenbakkersplein 23           
+|  Postcode  : 2834 EF                         
+|  Woonplaats: Dordrecht                          
+ ----------------------------------------------------"""
+
+print(adres)
+
+ 
