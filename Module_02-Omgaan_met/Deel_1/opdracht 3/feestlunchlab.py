2a70efb9032be511cdfc7564e92949b4ddc28b7c
CaSa078 <99073112@mydavinci.nl>
2022-09-12 11:30:15 +0200

    module 2 opdr 3erbij

---
+++ Module_02-Omgaan_met/Deel_1/opdracht 3/feestlunchlab.py
@@ -0,0 +1,5 @@
+croissant = 6.63
+stokbroden = 5.51
+kortsingbonnen = 1.50
+
+print(f"De feestlunch kost je bij de bakker 18.88 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!: {croissant + stokbroden - kortsingbonnen}")
---
+++ Module_02-Omgaan_met/Deel_1/opdracht 3/speelhallab.py
@@ -0,0 +1,12 @@
+# aatal vriend 3 met mij is 4 dus 4
+# 1 ticket kost 7,45 euro
+# Jullie willen ook met zijn allen voor 45 minuten in de VIP-VR-GameSeat.
+#De VIP-VR GameSeat kost per persoon 37 eurocent per 5 minuten. Jij trakteert dus betaal je alles.
+# dus 1 vip ticket kost 3.33 euro 
+# dus totaal kosten de vip tickets 13.32 euro
+alletickets = 29.80
+viptickets = 13.32
+totaal = alletickets + viptickets
+zin = f"Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar {round(totaal, 2)} euro"
+
+print(zin)
