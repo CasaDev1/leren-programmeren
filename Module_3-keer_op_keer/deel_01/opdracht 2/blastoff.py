3a517c82a7c35c8dd73dcf6d1b222d754ca67406
CaSa078 <99073112@mydavinci.nl>
2022-10-19 11:33:35 +0200

    blastoff

---
+++ Module_3-keer_op_keer/deel_01/opdracht 2/blastoff.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+print("de raketlancering gaat beginnen!!!")
+vertraging = 1
+for n in range(30,0,-1):
+    print(n)
+    sleep(vertraging)
+
+print("de raket is gelanceerd!!!!")
