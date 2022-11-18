1d87c8b101e3fb5110bea2bcd74a08768dd6b1c4
CaSa078 <99073112@mydavinci.nl>
2022-11-18 11:55:33 +0100

    wjdn

--- Module_3-keer_op_keer/deel_02/nogmaals/robotarm-python-2021-main/robotarm-python-2021-main/oefening9.py
+++ Module_3-keer_op_keer/deel_02/nogmaals/robotarm-python-2021-main/robotarm-python-2021-main/oefening9.py
@@ -1,12 +1,19 @@
 from RobotArm import RobotArm
 robotArm = RobotArm('exercise 9')
 # Jouw python instructies zet je vanaf hier:
-for b in range(11):
-    robotArm.grab()
-    for t in range(1,6): robotArm.moveRight()
-    robotArm.drop()
-    for e in range(1,6):robotArm.moveLeft()
-    if robotArm.grab() == False:
+
+skip_nummrers = [1,3,6,10]
+for k in range(0,10):
+    if k in skip_nummrers:
         robotArm.moveRight()
+    robotArm.grab()
+    for k in range(5):
+        robotArm.moveRight()
+    robotArm.drop()
+    for k in range(5):
+        robotArm.moveLeft()
+    
+
+        
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
