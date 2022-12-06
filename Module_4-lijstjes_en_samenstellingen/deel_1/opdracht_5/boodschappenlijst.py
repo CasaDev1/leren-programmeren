0da17171f2682c2484d701c26d44cb5c5554305d
CaSa078 <99073112@mydavinci.nl>
2022-12-06 22:50:56 +0100

    fruitmand

---
+++ Module_4-lijstjes_en_samenstellingen/deel_1/opdracht_5/boodschappenlijst.py
@@ -0,0 +1,20 @@
+boodschappen_tas_dic = {
+
+}
+tellertje = 1
+
+while True:
+    vraagboodschap = input("Voeg een product toe aan het lijst (typ hier): ")
+    if vraagboodschap in boodschappen_tas_dic:
+        boodschappen_tas_dic[vraagboodschap] += 1
+    else: 
+        boodschappen_tas_dic.update({vraagboodschap : tellertje})
+    voeg_product = input("wil je nog een product toe(typ hier):")
+    if voeg_product == "ja":
+        continue
+    else:
+        break
+print("[BOODSCHAPPENLIJST]")
+for key,value in boodschappen_tas_dic.items():
+    print(f"{value}x {key}")
+print("------------------------")
