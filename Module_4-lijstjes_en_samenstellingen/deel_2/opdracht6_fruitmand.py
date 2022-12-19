ac99d9f9eaa1232703026387c799053f1b029834
CaSa078 <99073112@mydavinci.nl>
2022-12-19 15:01:39 +0100

    update

--- Module_4-lijstjes_en_samenstellingen/deel_2/opdracht6_fruitmand.py
+++ Module_4-lijstjes_en_samenstellingen/deel_2/opdracht6_fruitmand.py
@@ -1,2 +1,5 @@
 from fruitmand import fruitmand
-print(fruitmand[1]['weight'])
+# print(fruitmand[1]['weight'])
+for fruit in fruitmand:
+    if fruit['name'] == 'appel':
+        print(fruit['weight'])
