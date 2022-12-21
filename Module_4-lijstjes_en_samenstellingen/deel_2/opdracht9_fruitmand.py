764537f20d092a4830e04598bc009432fc7ca6f3
CaSa078 <99073112@mydavinci.nl>
2022-12-21 10:22:46 +0100

    update

--- Module_4-lijstjes_en_samenstellingen/deel_2/opdracht9_fruitmand.py
+++ Module_4-lijstjes_en_samenstellingen/deel_2/opdracht9_fruitmand.py
@@ -1,12 +1,13 @@
 from fruitmand import fruitmand
-fruitmand.remove({'name' : 'druif','weight' : 5,'color' : 'red','round' : True})
 kleuren_lijst = []
+kleurenfruitmand = []
 for fruit in fruitmand:
-    if fruit['color'] not in kleuren_lijst:
-        kleuren_lijst.append(fruit['color'])
+    if fruit['name'] != 'druif':
+        kleurenfruitmand.append(fruit)
+        if fruit['color'] not in kleuren_lijst:
+            kleuren_lijst.append(fruit['color'])
 
 print(kleuren_lijst)
-
-    
+   
 
     
