9c859da307b084e61e3a4dd3fe63f48c3daa426a
CaSa078 <99073112@mydavinci.nl>
2023-01-13 13:13:12 +0100

    updated

--- Module_5-Meer_Fun(ctions)/deel_1/opdracht_3/multiplier-x.py
+++ Module_5-Meer_Fun(ctions)/deel_1/opdracht_3/multiplier-x.py
@@ -1,6 +1,6 @@
 def tafel_getal (aantal: int) -> str:
     for y in range(1,11):
-        print(f'{y} x {aantal} = {y * aantal}')
+        print(f'{aantal} x {y} = {y * aantal}')
 
 antwoord = int(input("Van welke getal wilt u de tafel zien?: "))
 tafel_getal(antwoord)
