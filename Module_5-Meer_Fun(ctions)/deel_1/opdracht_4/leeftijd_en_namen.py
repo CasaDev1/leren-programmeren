e6dd8a54212b6e58ff1d624c3ca0d179caa9ee35
CaSa078 <99073112@mydavinci.nl>
2023-01-23 13:43:23 +0100

    update

--- Module_5-Meer_Fun(ctions)/deel_1/opdracht_4/leeftijd_en_namen.py
+++ Module_5-Meer_Fun(ctions)/deel_1/opdracht_4/leeftijd_en_namen.py
@@ -1,18 +1,27 @@
-def namen_en_leeftijden():
-    namen = []
-    leeftijden = []
-    while True:
-        naamvraag = input("Wat is je naam? voer 'klaar'in als je klaar bent: ")
-        if naamvraag == "klaar":
-            break
-        leeftijdvraag = input("Wat is je leeftijd? type 'klaar' als je klaar bent: ")
-        if leeftijdvraag == "klaar":
-            break
-        namen.append(naamvraag)
-        leeftijden.append(leeftijdvraag)
-    n = zip(namen, leeftijden)
-    return n
+
+def namen_en_leeftijden() -> dict:
+    namen_leeftijd_dict = {}
+    naamvraag = input("Wat is je naam? voer 'klaar'in als je klaar bent: ")
+    if naamvraag == "klaar":
+        leeftijdvraag = 0
+    else:
+        leeftijdvraag = int(input("Wat is je leeftijd? type 'klaar' als je klaar bent: "))
+    namen_leeftijd_dict["naam"] = naamvraag
+    namen_leeftijd_dict["leeftijd"] = leeftijdvraag        
+    return namen_leeftijd_dict 
 
 
-for x,y in namen_en_leeftijden():
-    print(f"{x} is {y} jaar")
+
+namen_leeftijd_lijst = []
+     
+while True:
+    mijn_dict = namen_en_leeftijden()
+    if mijn_dict['naam'] != "klaar":
+        namen_leeftijd_lijst.append(mijn_dict)
+    else:
+        break    
+for y in namen_leeftijd_lijst:
+    name = y["naam"]
+    age = y["leeftijd"]
+    print(f"{name} is {age} jaar")
+    
