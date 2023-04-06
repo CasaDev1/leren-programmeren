e2f441f401f9c264f11162b3d3d3a277c111ae1b
CaSa078 <99073112@mydavinci.nl>
2023-04-06 15:38:33 +0200

    updated

--- Module_5-Meer_Fun(ctions)/deel_2/functions.py
+++ Module_5-Meer_Fun(ctions)/deel_2/functions.py
@@ -3,6 +3,8 @@ from termcolor import colored
 from data import JOURNEY_IN_DAYS
 from data import COST_FOOD_HUMAN_COPPER_PER_DAY
 from data import COST_FOOD_HORSE_COPPER_PER_DAY
+from data import COST_TENT_GOLD_PER_WEEK
+from data import COST_HORSE_SILVER_PER_DAY
 
 ##################### M04.D02.O2 #####################
 
